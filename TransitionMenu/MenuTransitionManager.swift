b7dbce3e7bc22ea0674b99d82b77471313d2d0ca
Stanley Pan <stanleypan.egre@gmail.com>
2016-11-05 12:09:50 +0900

    Get fadeIn transition working

---
+++ TransitionMenu/MenuTransitionManager.swift
@@ -0,0 +1,82 @@
+//
+//  MenuTransitionManager.swift
+//  TransitionMenu
+//
+//  Created by Stanley Pan on 05/11/2016.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class MenuTransitionManager: NSObject, UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate {
+    
+    fileprivate var presenting = false
+    
+    // MARK: UIViewControllerAnimatedTransitioning protocol methods
+    
+    // animate a change from one viewcontroller to another
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        // get reference to our fromView, toView and the container view that we should perform the transition in
+        let container = transitionContext.containerView
+        
+        // create a tuple of our screens
+        let screens : (from:UIViewController, to:UIViewController) = (transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!, transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)!)
+        
+        // assign references to our menu view controller and the 'bottom' view controller from the tuple
+        // remember that our menuViewController will alternate between the from and to view controller depending if we're presenting or dismissing
+        let menuViewController = !self.presenting ? screens.from as! MenuViewController : screens.to as! MenuViewController
+        let bottomViewController = !self.presenting ? screens.to as UIViewController : screens.from as UIViewController
+        
+        let menuView = menuViewController.view
+        let bottomView = bottomViewController.view
+        
+        // prepare the menu
+        if (self.presenting){
+            menuView?.alpha = 0
+        }
+        
+        // add the both views to our view controller
+        container.addSubview(bottomView!)
+        container.addSubview(menuView!)
+        
+        let duration = self.transitionDuration(using: transitionContext)
+        
+        // perform the animation!
+        UIView.animate(withDuration: duration, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: .curveEaseOut, animations: {
+            
+            // either fade in or fade out
+            menuView?.alpha = self.presenting ? 1 : 0
+            
+        }, completion: { finished in
+            
+            // tell our transitionContext object that we've finished animating
+            transitionContext.completeTransition(true)
+            
+            // bug: we have to manually add our 'to view' back http://openradar.appspot.com/radar?id=5320103646199808
+            UIApplication.shared.keyWindow?.addSubview(screens.to.view)
+            
+        })
+        
+    }
+    
+    // return how many seconds the transiton animation will take
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    // MARK: UIViewControllerTransitioningDelegate protocol methods
+    
+    // return the animataor when presenting a viewcontroller
+    // rememeber that an animator (or animation controller) is any object that aheres to the UIViewControllerAnimatedTransitioning protocol
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        self.presenting = true
+        return self
+    }
+    
+    // return the animator used when dismissing from a viewcontroller
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        self.presenting = false
+        return self
+    }
+}
